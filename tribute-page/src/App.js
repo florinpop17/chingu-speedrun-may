7ceaf5dfc8f43a616772c81d2e3e720ea9210aba
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-10 11:11:44 +0300

    added data from wikipedia

--- tribute-page/src/App.js
+++ tribute-page/src/App.js
@@ -5,24 +5,58 @@ class App extends Component {
     return (
         <div className="container-fluid">
             <div className="row">
-                <nav className="navbar navbar-default navbar-static-top">
-                    <div className="navbar-header">
-
+                <div className="col-md-6 col-md-offset-3">
+                    <div className="jumbotron">
+                        <h2 className="text-center">Desmond Doss</h2>
+                        <div className="thumbnail">
+                            <img className="text-center" src="https://upload.wikimedia.org/wikipedia/commons/7/71/DossDesmondT_USArmy.jpg" alt=""/>
+                            <p className="caption text-center">Doss about to receive the Medal of Honor in October 1945</p>
+                        </div>
                     </div>
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <a href="#">Link</a>
-                        </li>
-                        <li>
-                            <a href="#home">Link</a>
-                        </li>
-                        <li>
-                            <a href="#home">Link</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="col-md-12">
-                    <h2>Hello to React App</h2>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h3>Early life</h3>
+                            <p>
+                                Desmond Doss was born in Lynchburg, Virginia, to William Thomas Doss, a carpenter, and Bertha Edward Doss (née Oliver),
+                                a homemaker and shoe factory worker. His mother raised him as a devout
+                                Seventh-day Adventist and instilled Sabbath-keeping, nonviolence, and a vegetarian lifestyle in his upbringing.
+                                He grew up in the Fairview Heights area of Lynchburg, Virginia alongside his older sister Audrey and younger brother Harold.
+                                Doss attended the Park Avenue Seventh-day Adventist Church school until the eighth grade, and subsequently
+                                found a job at the Lynchburg Lumber Company to support his family during the Great Depression.
+                            </p>
+                            <h3>World War II service</h3>
+                            <p>
+                                Before the outbreak of World War II, Doss was employed as a joiner at a shipyard in Newport News, Virginia.
+                                Doss entered military service – despite being offered a deferment for his shipyard work – on April 1, 1942 at Camp Lee, Virginia.
+                                He was sent to Fort Jackson in South Carolina for training with the reactivated 77th Infantry Division.
+                                Meanwhile, his brother Harold served aboard the USS Lindsey. Doss refused to kill an enemy soldier or carry a weapon
+                                into combat because of his personal beliefs as a Seventh-day Adventist.
+                                He consequently became a medic assigned to 2nd Platoon, B Company, 1st Battalion, 307th Infantry,
+                                77th Infantry Division. While serving with his platoon in 1944 on Guam and the Philippines,
+                                he was awarded a Bronze Star Medal for aiding wounded soldiers under fire. During the Battle of Okinawa,
+                                he saved the lives of 75 wounded infantrymen[DTD 1] atop the Maeda Escarpment. Doss was wounded four times in Okinawa,
+                                and was evacuated on May 21, 1945 aboard the USS Mercy.
+                            </p>
+                            <h3>Honors and recognition</h3>
+                            <ul>
+                                <li>A portion of US Route 501 near Peaks View Park is named "Pfc. Desmond T. Doss Memorial Expressway." Local veterans of the area honor him by decorating the signs marking this portion of road several times during the year, particularly around patriotic holidays.</li>
+                                <li>In 1951, Camp Desmond T. Doss was created in Grand Ledge, Michigan to help train young Seventh-day Adventist men for service in the military. The camp was active throughout the Korean and Vietnam wars before the property was sold in 1988.</li>
+                                <li>In the early 1980s, a school in Lynchburg was renamed Desmond T. Doss Christian Academy. The school was founded by the Lynchburg Seventh-Day Adventist Church, the home church of Desmond Doss during his years in Lynchburg. The church wanted to honor Doss for standing strong in his faith despite facing great adversity.[20] Doss visited the school that bears his name three times before his death.</li>
+                                <li>On July 10, 1990, a section of Georgia Highway 2 between US Highway 27 and Georgia Highway 193 in Walker County was named the "Desmond T. Doss Medal of Honor Highway."</li>
+                                <li>On March 20, 2000, Doss appeared before the Georgia House of Representatives and was presented a special resolution honoring his heroic accomplishments on behalf of the country.</li>
+                                <li>On July 4, 2004, a statue of Doss was dedicated at the National Museum of Patriotism in Atlanta, Georgia, which remained until the museum's closure in July 2010.</li>
+                                <li>In May 2007, a statue of Doss was dedicated at Veterans Memorial Park in Collegedale, Tennessee.</li>
+                                <li>In July 2008, the guest house at Walter Reed Army Medical Center in Washington, D.C., was renamed Doss Memorial Hall.</li>
+                                <li>On August 30, 2008, a two-mile stretch of Alabama Highway 9 in Piedmont was named the "Desmond T. Doss Sr. Memorial Highway."</li>
+                                <li>On October 25, 2016, the City of Lynchburg, Virginia, awarded a plaque in his honor to Desmond T. Doss Christian Academy.</li>
+                                <li>On February 7, 2017, PETA posthumously honored Doss with a Hero to Animals award in recognition of his lifelong commitment to vegetarianism.</li>
+                            </ul>
+                            <h3>If you want to read more about Desmond Doss, click <a href="https://en.wikipedia.org/wiki/Desmond_Doss">here</a>.</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

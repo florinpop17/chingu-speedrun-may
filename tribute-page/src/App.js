89c1880585b2c47550059f273ca1e641f6589926
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-10 10:58:04 +0300

    bootswatch

--- tribute-page/src/App.js
+++ tribute-page/src/App.js
@@ -3,8 +3,24 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
+                <nav className="navbar navbar-default navbar-static-top">
+                    <div className="navbar-header">
+
+                    </div>
+                    <ul className="nav navbar-nav">
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                        <li>
+                            <a href="#home">Link</a>
+                        </li>
+                        <li>
+                            <a href="#home">Link</a>
+                        </li>
+                    </ul>
+                </nav>
                 <div className="col-md-12">
                     <h2>Hello to React App</h2>
                 </div>

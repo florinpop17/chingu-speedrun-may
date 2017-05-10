a57f2d1ea22414fe420e33099f9adf9447349e14
Florin Pop <popflorin1705@yahoo.com>
2017-05-10 23:57:21 +0300

    simple portfolio page

--- speedrun-portfolio/src/App.js
+++ speedrun-portfolio/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import projects from './projects';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			projects
+		}
+	}
+
 	render() {
+		const { projects } = this.state;
+
 		return (
-			<div className="container-fluid">
+			<div className="container">
 				<div className="row">
 					<div className="col-md-12">
-						<h2>Hello to React App</h2>
+						<h2 className="text-center">Portfolio website</h2>
+						<ul>
+							{ projects ? projects.map((project, idx) => (
+								<li key={idx}>
+									<a href={ '/'+project+'/' }>{ project }</a>
+								</li>
+							)) : '' }
+						</ul>
 					</div>
 
-					<footer className="nav navbar-inverse navbar-fixed-bottom">
+					<footer className="nav navbar-default navbar-fixed-bottom">
 						<div className="container">
 							<p className="text-center">Create with <i className="fa fa-heart"></i> by <a target="_blank" href="http://www.florin-pop.com">Florin Pop</a>. Github <a target="_blank" href="https://github.com/florinpop17/chingu-speedrun-may">repo</a>.</p>
 						</div>
---
+++ speedrun-portfolio/src/projects.js
@@ -0,0 +1,7 @@
+export default [
+	'tribute-page',
+	'random-quote-machine',
+	'local-weather',
+	'wikipedia-viewer',
+	'twitch-client'
+]

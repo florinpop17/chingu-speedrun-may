371047065de1277067bf490816f73406479c1ea6
Florin Pop <popflorin1705@yahoo.com>
2017-05-10 21:09:37 +0300

    panel for rendering articles

--- wikipedia-viewer/src/App.js
+++ wikipedia-viewer/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
 	constructor() {
@@ -13,9 +14,18 @@ class App extends Component {
 
 	getWiki() {
 		const query = this.refs.search.value || 'Main Page';
+
 		axios.get(`https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&generator=search&prop=extracts|info&inprop=url&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${query}`)
 			.then(res => {
-				console.log(res.data);
+				let articles = [];
+				const { pages } = res.data.query;
+				for(let key in pages){
+					const { title, fullurl: url, extract } = pages[key];
+
+					articles.push({ title, url, extract });
+				}
+
+				this.setState({ articles });
 			})
 	}
 
@@ -34,18 +44,34 @@ class App extends Component {
 							<div className="col-md-12">
 								<div className="form-group">
 									<h3>Search wikipedia: <small>(searches on input change)</small></h3>
-									<input type="text" className="form-control form-wi"/>
+									<input type="text" ref="search" onChange={this.getWiki} className="form-control form-wi"/>
 									<a target="_blank" href="https://en.wikipedia.org/wiki/Special:Random" className="btn btn-primary pull-right"><i className="fa fa-random"></i></a>
 								</div>
 							</div>
 						</div>
 					</div>
 
-					{ articles ? articles.map(article => (
-						<div className="panel panel-primary">
-
+					{ articles ? (
+						<div className="container">
+							<div className="row">
+								<div className="col-md-12">
+									{
+										articles.map((article, idx) => (
+											<div key={idx} className="panel panel-primary">
+												<div className="panel-heading">
+													{ article.title }
+												</div>
+												<div className="panel-body">
+													<p>{ article.extract }</p>
+													<a href={ article.url }>Read more</a>
+												</div>
+											</div>
+										))
+									}
+								</div>
+							</div>
 						</div>
-					)) : '' }
+					) : '' }
 
 					<footer className="nav navbar-inverse navbar-fixed-bottom">
 						<div className="container">

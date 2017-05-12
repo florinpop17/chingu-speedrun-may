54e25fa6497551c86f7bab6b7de77ec7948a53cc
Florin Pop <popflorin1705@yahoo.com>
2017-05-12 18:31:48 +0300

    game logic + design

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
 
-import Game from './Game';
+import Board from './Board';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			cells : ['', '', '', '', '', '', '', '', ''],
+			current_player: 'X',
+			winner: undefined,
+			winning_combo: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+		}
+
+		this.handleClick = this.handleClick.bind(this);
+		this.checkWin = this.checkWin.bind(this);
+	}
+
+	handleClick(id) {
+		let { cells, current_player, winner } = this.state;
+
+		if(!winner) {
+			if(!cells[id]){
+				cells[id] = current_player;
+				current_player = current_player === 'X' ? 'O' : 'X';
+			}
+
+			this.setState({ cells, current_player });
+			this.checkWin();
+		}
+	}
+
+	checkWin() {
+		let { cells, winning_combo, winner } = this.state;
+
+		for(let combo of winning_combo){
+			if(cells[combo[0]] && cells[combo[1]] && cells[combo[2]]) { // cells aren't empty
+				if(cells[combo[0]] === cells[combo[1]] && cells[combo[1]] === cells[combo[2]]){
+					winner = cells[combo[0]];
+					this.setState({ winner });
+					break;
+				}
+			}
+		}
+	}
+
+	resetGame() {
+		this.setState({
+			cells: ['', '', '', '', '', '', '', '', ''],
+			winner: undefined,
+			current_player: 'X'
+		});
+	}
+
 	render() {
+		const { cells, winner } = this.state;
+
 		return (
 			<div className="container-fluid">
 				<div className="row">
 					<div className="col-md-12">
-						<Game />
+						<Board cells={cells} check={this.handleClick}/>
+						{ winner ? (
+							<div className="text-center">
+								<p>{ winner } won the game.</p>
+								<button onClick={() => this.resetGame()} className="btn btn-primary">Reset</button>
+							</div>
+						) : '' }
 					</div>
 
 					<footer className="nav navbar-defaul navbar-fixed-bottom">

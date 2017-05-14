492e6e5eb7e12a8cd7084269809390a2d45509e8
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-14 19:27:10 +0300

    init

---
+++ simon-game/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-md-12">
+						<h2>Hello to React App</h2>
+					</div>
+
+					<footer className="nav navbar-inverse navbar-fixed-bottom">
+						<div className="container">
+							<p className="text-center">Create with <i className="fa fa-heart"></i> by <a target="_blank" href="http://www.florin-pop.com">Florin Pop</a>. Github <a target="_blank" href="https://github.com/florinpop17/chingu-speedrun-may">repo</a>.</p>
+						</div>
+					</footer>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;

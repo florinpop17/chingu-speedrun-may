d2b0ff43c33534784cb648ecd93b5b4f747f03bc
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-14 19:51:51 +0300

    init

---
+++ simon-game/src/SimonGame.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SimonGame extends Component {
+
+    render() {
+        return (
+            <div className="col-md-12">
+                H1
+            </div>
+        )
+    }
+}
+
+export default SimonGame;

6c8d44a214e128d7e98c5513466a4b4403421fe0
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-14 14:56:41 +0300

    draw verification

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -10,12 +10,14 @@ class App extends Component {
 			cells : ['', '', '', '', '', '', '', '', ''],
 			current_player: 'X',
 			winner: undefined,
+			draw: false,
 			won_combo: [0, 0, 0, 0, 0, 0, 0, 0, 0],
 			winning_combo: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 		}
 
 		this.handleClick = this.handleClick.bind(this);
 		this.checkWin = this.checkWin.bind(this);
+		this.checkDraw = this.checkDraw.bind(this);
 	}
 
 	handleClick(id) {
@@ -28,7 +30,8 @@ class App extends Component {
 			}
 
 			this.setState({ cells, current_player });
-			this.checkWin();
+			if(!this.checkWin()) // checking if there was a winner, if no, check if the game is full
+				this.checkDraw();
 		}
 	}
 
@@ -45,23 +48,36 @@ class App extends Component {
 					won_combo[combo[2]] = 1;
 
 					this.setState({ winner, won_combo });
-					break;
+					return true;
 				}
 			}
 		}
+
+		return false;
+	}
+
+	checkDraw() {
+		let { cells } = this.state;
+		if(cells.indexOf('') === -1){
+			this.setState({ draw: true });
+			return true;
+		}
+
+		return false;
 	}
 
 	resetGame() {
 		this.setState({
 			cells: ['', '', '', '', '', '', '', '', ''],
 			winner: undefined,
+			draw: false,
 			current_player: 'X',
 			won_combo: [0, 0, 0, 0, 0, 0, 0, 0, 0]
 		});
 	}
 
 	render() {
-		const { cells, winner, won_combo } = this.state;
+		const { cells, winner, draw, won_combo } = this.state;
 
 		return (
 			<div className="container-fluid">
@@ -71,6 +87,11 @@ class App extends Component {
 						{ winner ? (
 							<div className="text-center">
 								<p>{ winner } won the game.</p>
+								<button onClick={() => this.resetGame()} className="btn btn-primary">Reset</button>
+							</div>
+						) : draw ? (
+							<div className="text-center">
+								<p>Game is draw!</p>
 								<button onClick={() => this.resetGame()} className="btn btn-primary">Reset</button>
 							</div>
 						) : '' }

dfb4dc6051fa0bc710c90ba4797ef5ead63d135d
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-15 10:03:06 +0300

    game logic implemented

--- simon-game/src/SimonGame.js
+++ simon-game/src/SimonGame.js
@@ -5,15 +5,20 @@ class SimonGame extends Component {
         super();
 
         this.state = {
+            total_games: 3,
             started: false,
-            level: 5,
+            level: 1,
             strict: false,
             running: false,
             game: [],
-            player: []
+            player: [],
+            message: '',
+            won: false,
+            lost: false
         }
 
         this.timed = this.timed.bind(this);
+        this.sing = this.sing.bind(this);
     }
 
     // green  => 0
@@ -22,31 +27,85 @@ class SimonGame extends Component {
     // yellow => 3
 
     btnClick(color) {
-        let { started, running } = this.state;
+        let { started, running, game, player, strict, level, total_games } = this.state;
         let { a_g, a_r, a_b, a_y, d_g, d_r, d_b, d_y } = this.refs;
 
         if(started && !running) {
             if(color === 'g'){
-                console.log('Green pressed.');
+                player.push(0)
                 a_g.play();
                 d_g.className += ' active';
             }
             if(color === 'b'){
-                console.log('Blue pressed.');
+                player.push(1)
                 a_b.play();
                 d_b.className += ' active';
             }
             if(color === 'r'){
-                console.log('Red pressed.');
+                player.push(2)
                 a_r.play();
                 d_r.className += ' active';
             }
             if(color === 'y'){
-                console.log('Yellow pressed.');
+                player.push(3)
                 a_y.play();
                 d_y.className += ' active';
             }
+
+            if(!this.isCorrectGame(game, player)){
+                if(strict){
+                    this.setState({
+                        game: [],
+                        player: [],
+                        message: 'You lost. Please start over!',
+                        running: true,
+                        level: 0,
+                        lost: true
+                    });
+                } else {
+                    this.setState({
+                        message: 'Wrong... Try again.',
+                        player: [],
+                        running: true
+                    })
+                    setTimeout(this.sing, 2000);
+                }
+            } else {
+                if(player.length === level){
+                    level += 1;
+                    player = [];
+
+                    if(level > total_games){
+                       // game ended, you won
+                       this.setState({
+                           message: 'Congrats. You won the game!',
+                           running: true,
+                           level: 0,
+                           game: [],
+                           player: [],
+                           won: true
+                       });
+                    }  else {
+                        this.setState({
+                            message: `You completed level ${level - 1}!`, // you completed previous level
+                            level,
+                            player
+                        });
+
+                        setTimeout(this.sing, 2000);
+                    }
+                }
+                this.setState({
+                    player
+                })
+            }
         }
+    }
+
+    isCorrectGame(game, player) {
+        return player.every((pl, idx) => {
+            return pl === game[idx];
+        })
     }
 
     changeStrict() {
@@ -58,30 +117,44 @@ class SimonGame extends Component {
     }
 
     startGame() {
-        let { init } = this.state;
+        let { total_games } = this.state;
         let game = [];
 
-        for(var i=0; i<20; i++) { // 20 games
+        for(var i=0; i<total_games; i++) {
             game.push(Math.floor(Math.random() * 4));
         }
 
-        setTimeout(() => {
-            const { level, game } = this.state;
+        setTimeout(this.sing, 1000);
 
-            if(level < game.length){
-                for(var i=0; i<level; i++){
-                    this.timed(i);
-                }
-            } else {
-                // game ended, you won
-                this.setState({  })
-            }
-        }, 1000);
-
-        this.setState({ game, started: true, init });
+        this.setState({
+            game,
+            won: false,
+            lost: false,
+            level: 1,
+            started: true,
+            message: 'Listen now...'
+        });
     }
 
-    timed(i) {
+    sing() {
+        const { level, game } = this.state;
+
+        if(level <= game.length){
+            this.setState({
+                running: true, // game running, listen now...
+                message: 'Listen now...'
+            });
+
+            for(var i=0; i<level; i++){
+                if(i === level-1) // the last tile, change running to false
+                    this.timed(i, true);
+                else
+                    this.timed(i, false);
+            }
+        }
+    }
+
+    timed(i, last) {
         const { game } = this.state;
         const { a_g, a_r, a_b, a_y, d_g, d_r, d_b, d_y } = this.refs;
 
@@ -89,28 +162,31 @@ class SimonGame extends Component {
             let current = game[i];
 
             if(current === 0){
-                console.log('Green played.');
                 a_g.play();
                 d_g.className += ' active';
             }
             if(current === 1){
-                console.log('Blue played.');
                 a_b.play();
                 d_b.className += ' active';
             }
             if(current === 2){
-                console.log('Red played.');
                 a_r.play();
                 d_r.className += ' active';
             }
             if(current === 3){
-                console.log('Yellow played.');
                 a_y.play();
                 d_y.className += ' active';
             }
-        }, 1000 * i, () => {
-            console.log('timeout ended');
-        });
+
+            if(last) {
+                setTimeout(() => {
+                    this.setState({
+                        running: false, // user can type now
+                        message: 'Your turn.'
+                    });
+                }, 1000);
+            }
+        }, 1000 * i);
     }
 
     resetGame() {
@@ -121,6 +197,7 @@ class SimonGame extends Component {
         let { a_g, a_r, a_b, a_y, d_g, d_r, d_b, d_y } = this.refs;
 
         // remove active class from tiles when sound is ended
+        // with the onended event
 
         a_g.onended = () => {
             d_g.className = d_g.className.replace(' active', '');
@@ -137,7 +214,7 @@ class SimonGame extends Component {
     }
 
     render() {
-        const { started, level, strict } = this.state;
+        const { started, level, strict, message, won, lost } = this.state;
 
         return (
             <div className="col-md-12">
@@ -156,8 +233,13 @@ class SimonGame extends Component {
                             </div>
                         ) : (
                             <div className="text-center">
-                                <h4>Level: { level }</h4>
-                                <button onClick={() => this.resetGame()} className="btn btn-warning">Stop</button>
+                                <h4>{ level ? `Level: ${level}` : ''}</h4>
+                                <p>{ message }</p>
+                                { won || lost ? (
+                                    <button onClick={() => this.resetGame()} className="btn btn-primary">Play again</button>
+                                ) : (
+                                    <button onClick={() => this.resetGame()} className="btn btn-warning">Stop</button>
+                                ) }
                             </div>
                         ) }
                     </div>

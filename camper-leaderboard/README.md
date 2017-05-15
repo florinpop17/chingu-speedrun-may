979901f33096cc79b64112d5cf3c83d9241aaf99
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-15 11:57:47 +0300

    styling + imported data

--- camper-leaderboard/README.md
+++ camper-leaderboard/README.md
@@ -1,17 +1,25 @@
-# Build a Tribute Page
+# Build a Camper Leaderboard
 
 ## Objective
 
-Build an app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/NNvBQW/.
+Build an app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/eZGMjp/.
 
 ## Requirements
 
-* User Story: I can view a tribute page with an image and text.
+* User Story: I can see a table of the Free Code Camp campers who've earned the most brownie points in the past 30 days.
 
-* User Story: I can click on a link that will take me to an external website with further information on the topic.
+* User Story: I can see how many brownie points they've earned in the past 30 days, and how many they've earned total.
+
+* User Story: I can toggle between sorting the list by how many brownie points they've earned in the past 30 days and by how many brownie points they've earned total.
+
+**Hint:** To get the top 100 campers for the last 30 days: https://fcctop100.herokuapp.com/api/fccusers/top/recent.
+
+**Hint:** To get the top 100 campers of all time: https://fcctop100.herokuapp.com/api/fccusers/top/alltime.
 
 ## Notes
 
 * Used create-react-app to scaffold the application
 
 * Twitter-bootstrap for the styling
+
+* Readable bootswatch theme
--- camper-leaderboard/src/App.js
+++ camper-leaderboard/src/App.js
@@ -1,15 +1,79 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+	state = {
+		alltime: [],
+		recent: [],
+		show: 'alltime'
+	}
+
+	componentDidMount() {
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+			.then(res => {
+				if(res.status === 200){
+					this.setState({ recent: res.data })
+				}
+			});
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+			.then(res => {
+				if(res.status === 200){
+					this.setState({ alltime: res.data })
+				}
+			});
+	}
+
+	changeView() {
+		let { show } = this.state;
+
+		if(show === 'alltime') show = 'recent';
+		else show = 'alltime';
+
+		this.setState({ show });
+	}
+
 	render() {
+		const { show, alltime, recent } = this.state;
+
 		return (
 			<div className="container-fluid">
 				<div className="row">
 					<div className="col-md-12">
-						<h2>Hello to React App</h2>
+						<h2>Leaderboard <button onClick={() => { this.changeView() }} className="btn btn-primary btn-sm pull-right">{ show === 'alltime' ? 'All time' : 'Last 30' }</button></h2>
+						<table className="table table-striped table-bordered">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Camper</th>
+									<th>Last 30 days points</th>
+									<th>All time points</th>
+								</tr>
+							</thead>
+							<tbody>
+								{ show === 'alltime' ? (
+									alltime.map((camper, idx) => (
+										<tr key={ idx }>
+											<td>{ idx + 1 }</td>
+											<td><img src={ camper.img } alt={ camper.username }/> <strong>{ camper.username }</strong></td>
+											<td>{ camper.recent }</td>
+											<td>{ camper.alltime }</td>
+										</tr>
+									))
+								) : (
+									recent.map((camper, idx) => (
+										<tr key={ idx }>
+											<td>{ idx + 1 }</td>
+											<td><img src={ camper.img } alt={ camper.username }/> <strong>{ camper.username }</strong></td>
+											<td>{ camper.recent }</td>
+											<td>{ camper.alltime }</td>
+										</tr>
+									))
+								) }
+							</tbody>
+						</table>
 					</div>
 
-					<footer className="nav navbar-inverse navbar-fixed-bottom">
+					<footer className="nav navbar-default navbar-fixed-bottom">
 						<div className="container">
 							<p className="text-center">Create with <i className="fa fa-heart"></i> by <a target="_blank" href="http://www.florin-pop.com">Florin Pop</a>. Github <a target="_blank" href="https://github.com/florinpop17/chingu-speedrun-may">repo</a>.</p>
 						</div>
